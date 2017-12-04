8c58481c7b9404db776aead0c9a243bac90d07a3
samuel <samuelantequeraworker@gmail.com>
2017-12-04 11:24:19 +0100

    Arreglado Login Resize problem

--- src/view/Tasker.java
+++ src/view/Tasker.java
@@ -17,6 +17,7 @@ public class Tasker extends Application{
         Parent root = FXMLLoader.load(getClass().getResource("FXMLLogin.fxml"));
         
         Scene scene = new Scene(root);
+        stage.setResizable(false);
         
         stage.setScene(scene);
         stage.show();
